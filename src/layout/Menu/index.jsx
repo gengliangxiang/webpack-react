0256358bbbbf455e4822d03c0f95912dfa69689d
耿良祥 <gengliangxiang@gengliangxiangdeMacBook-Pro.local>
2020-05-22 10:21:42 +0800

    菜单展开收缩处理

--- src/layout/Menu/index.jsx
+++ src/layout/Menu/index.jsx
@@ -26,7 +26,6 @@ const muneData = [
 	{
 		primary: 'webpack',
 		link: '/webpack',
-		isOpen: false,
 		icon: 'inbox',
 		items: [
 			{
@@ -50,72 +49,95 @@ const muneData = [
 
 export default function NestedList() {
 	const [selected, setselected] = React.useState('/home');
+	const [open, setOpen] = React.useState('');
 
 	const itemClick = item => {
 		const data = { ...item };
-		if ('isOpen' in data) {
-			item.isOpen = !item.isOpen;
+		if (data.items && data.items.length) {
+			const nowOpen = open !== data.primary ? data.primary : '';
+			setOpen(nowOpen);
 		} else {
 			setselected(item.link);
 		}
 	};
 
 	const mune = muneData.map(data => (
-		<div key={data.link}>
-			<ListItem
-				button
-				// component="div"
-				component={Link}
-				to={data.link}
-				className={
-					selected === data.link
-						? style.listItemSelected
-						: style.listItem
-				}
-				onClick={() => itemClick(data)}
-			>
-				<ListItemIcon>
-					<Icon className={style.itemIcon}>{data.icon}</Icon>
-				</ListItemIcon>
-				<ListItemText primary={data.primary} />
-				{!!data.items && (data.isOpen ? expandLess : expandMore)}
-			</ListItem>
+		<div key={data.primary}>
+			{!data.items && (
+				<ListItem
+					button
+					component={Link}
+					to={data.link}
+					className={
+						selected === data.link
+							? style.listItemSelected
+							: style.listItem
+					}
+					onClick={() => itemClick(data)}
+				>
+					<ListItemIcon>
+						<Icon className={style.itemIcon}>{data.icon}</Icon>
+					</ListItemIcon>
+					<ListItemText primary={data.primary} />
+				</ListItem>
+			)}
 			{!!data.items && (
-				<Collapse in={data.isOpen} timeout="auto" unmountOnExit>
-					<List
-						component="div"
-						disablePadding
-						className={style.nested}
+				<>
+					<ListItem
+						button
+						className={style.listItem}
+						onClick={() => itemClick(data)}
 					>
-						{data.items.map(items => (
-							<ListItem
-								button
-								key={items.link}
-								component={Link}
-								to={items.link}
-								className={
-									selected === items.link
-										? style.listChildrenSelected
-										: style.listChildrenItem
-								}
-								onClick={() => itemClick(items)}
-							>
-								<ListItemIcon>
-									<Icon className={style.itemIcon}>
-										{items.icon}
-									</Icon>
-								</ListItemIcon>
-								<ListItemText primary={items.primary} />
-							</ListItem>
-						))}
-					</List>
-				</Collapse>
+						<ListItemIcon>
+							<Icon className={style.itemIcon}>{data.icon}</Icon>
+						</ListItemIcon>
+						<ListItemText primary={data.primary} />
+						{open === data.primary ? expandLess : expandMore}
+					</ListItem>
+					<Collapse
+						in={open === data.primary}
+						timeout="auto"
+						unmountOnExit
+					>
+						<List
+							component="div"
+							disablePadding
+							className={style.nested}
+						>
+							{data.items.map(items => (
+								<ListItem
+									button
+									key={items.link}
+									component={Link}
+									to={items.link}
+									className={
+										selected === items.link
+											? style.listChildrenSelected
+											: style.listChildrenItem
+									}
+									onClick={() => itemClick(items)}
+								>
+									<ListItemIcon>
+										<Icon className={style.itemIcon}>
+											{items.icon}
+										</Icon>
+									</ListItemIcon>
+									<ListItemText primary={items.primary} />
+								</ListItem>
+							))}
+						</List>
+					</Collapse>
+				</>
 			)}
 		</div>
 	));
 
 	return (
-		<List component="nav" className={style.navRoot}>
+		<List
+			component="nav"
+			aria-labelledby="nested-list-subheader"
+			className={style.navRoot}
+		>
 			{mune}
 		</List>
 	);
